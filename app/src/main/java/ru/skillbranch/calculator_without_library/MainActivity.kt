8d2df77d8d5201cceaa0db0ac68b6543fa7cf748
Artyom Shklyarskiy <tw1nkyy@yandex.ru>
2020-03-04 21:04:32 +0700

    I tried

--- app/src/main/java/ru/skillbranch/calculator_without_library/MainActivity.kt
+++ app/src/main/java/ru/skillbranch/calculator_without_library/MainActivity.kt
@@ -1,24 +1,46 @@
 package ru.skillbranch.calculator_without_library
 
 import android.os.Bundle
+import android.os.Parcel
+import android.os.Parcelable
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.activity_main.result
 
 
-class MainActivity : AppCompatActivity() {
-
+class MainActivity() : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         btn_result.setOnClickListener {
             val oper = enter.text.toString()
-            val calc = StringCalculator()
-
-            result.text = calc.calculate(operation = oper)
+          //  if (cheak(oper)) {
+                val calc = StringCalculator()
+                result.text = calc.calculate(oper)
+           // } else result.text = "Неправильно введено выражение"
         }
     }
-}
+
+
+
+
+        fun cheak(str: String): Boolean {
+            var ch = 0
+            for (i in 0..str.length) {
+                if (str[i] == '.' && ch == 0) {
+                    ch++
+                } else if (str[i] == '.' && ch != 0)
+                    return false
+                else if (str[i] == '+' || str[i] == '-' || str[i] == '*' || str[i] == '/') {
+                    if (str[i + 1] == '+' || str[i + 1] == '-' || str[i + 1] == '*' || str[i + 1] == '/' || str[i - 1] == '+' || str[i - 1] == '-' || str[i - 1] == '*' || str[i - 1] == '/')
+                        return false
+                }
+            }
+
+            return true
+        }
+
+    }
+
 
